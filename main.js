8ff2b5899b1eaedeac1406dbf72d83fadc475f85
Thomas Rory Gummerson <trgwii@hotmail.com>
2016-10-13 19:06:20 +0200

    More changes

--- main.js
+++ main.js
@@ -1,11 +1,34 @@
 'use strict';
 
+const fs = require('fs');
+
 const { BotAPI } = require('teleapiwrapper');
+const Discord = require('discord.io');
+const updateGetter = require('./updategetter.js');
 
 const settings = JSON.parse(fs.readFileSync('settings.json'));
 
-const bot = new BotAPI(settings.telegram.auth.token);
+const tgBot = new BotAPI(settings.telegram.auth.token);
 
-bot.getUpdates().then((err, updates) => {
-	
+const tgBotUpdates = updateGetter(tgBot, settings.telegram.timeout);
+
+tgBotUpdates.on('update', update => {
+	console.log(update);
 });
+
+if (settings.debug.port) {
+	process.stdin.setEncoding('utf8');
+	process.stdin.on('data', data => {
+		data = data.trim();
+		switch (data) {
+			case 'exit':
+				tgBotUpdates.stop();
+				process.stdin.pause();
+				console.log('Exiting...');
+				break;
+			default:
+				break;
+		}
+			
+	});
+}
