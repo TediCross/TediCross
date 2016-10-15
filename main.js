35b5e22c5fba4c9491f71c7830cac441d7934f83
Thomas Rory Gummerson <trgwii@hotmail.com>
2016-10-15 16:34:05 +0200

    Giant changes urghghhg

--- main.js
+++ main.js
@@ -1,34 +1,113 @@
 'use strict';
 
+const stream = require('stream');
 const fs = require('fs');
+const http = require('http');
+const https = require('https');
 
-const { BotAPI } = require('teleapiwrapper');
+const { BotAPI, InputFile } = require('teleapiwrapper');
 const Discord = require('discord.io');
 const updateGetter = require('./updategetter.js');
 
 const settings = JSON.parse(fs.readFileSync('settings.json'));
 
 const tgBot = new BotAPI(settings.telegram.auth.token);
-
-const tgBotUpdates = updateGetter(tgBot, settings.telegram.timeout);
-
-tgBotUpdates.on('update', update => {
-	console.log(update);
+const dcBot = new Discord.Client({
+	token: settings.discord.auth.token,
+	autorun: true
 });
 
-if (settings.debug.port) {
+updateGetter(tgBot, settings.telegram.timeout);
+
+// Log inits
+dcBot.on('ready', () => console.log(`Discord: ${dcBot.username} (${dcBot.id})`));
+tgBot.getMe().then(bot => console.log(`Telegram: ${bot.username} (${bot.id})`));
+
+const dcUsers = JSON.parse(fs.readFileSync(settings.discord.usersfile, 'utf8'));
+setInterval(() => fs.writeFile(settings.discord.usersfile, JSON.stringify(dcUsers, undefined, '\t')), 30000);
+
+const logStream = fs.createWriteStream('debug_log.txt', { flags: 'a' });
+dcBot.on('any', e => {
+	if (e.t === 'GUILD_CREATE') {
+		e.d.members.forEach(member => {
+			dcUsers[member.user.id] = member.user.username;
+		});
+	}
+	else if (e.t === 'MESSAGE_CREATE') {
+		if(e.d.attachments) {
+			e.d.attachments.forEach(attachment => {
+				const req = attachment.url[4] === 's' ? https.get(attachment.url) : http.get(attachment.url);
+				req.on('response', res => {
+					const s = new stream.PassThrough();
+					tgBot.sendPhoto({
+						chat_id: settings.telegram.chat_id,
+						photo: new InputFile(s, attachment.url.split('/').pop())
+					});
+					res.pipe(s);
+				});
+			});
+		}
+	}
+	logStream.write(JSON.stringify(e, undefined, '\t') + '\n\n\n');
+});
+dcBot.on('presence', (user, userID) => { dcUsers[userID] = user; });
+dcBot.on('message', (user, userID, channelID, message, event) => {
+	dcUsers[userID] = user;
+	// Ignore own messages
+	if (userID !== settings.discord.botID) {
+		if (settings.debug) {
+			console.log(`Got message: \`${message}\` from Discord-user: ${user} (${userID})`);
+		}
+		tgBot.sendMessage({
+			chat_id: settings.telegram.chat_id,
+			text: `<b>${user}</b>: ${message
+				.replace(/</g, '&lt;')
+				.replace(/>/g, '&gt;')
+				.replace(/&/g, '&amp;')
+				.replace(/\*\*([^*]+)\*\*/g, (m, b) => '<b>' + b + '</b>')
+				.replace(/\*([^*]+)\*/g, (m, b) => '<i>' + b + '</i>')
+				.replace(/_([^*]+)_/g, (m, b) => '<i>' + b + '</i>')
+				.replace(/<@!(\d+)>/g, (m, id) => {
+					if (dcUsers[id]) {
+						return `@${dcUsers[id]}`;
+					} else {
+						return m;
+					}
+				})}`,
+			parse_mode: 'HTML'
+		});
+	}
+});
+
+tgBot.on('text', message => {
+	if (settings.debug) {
+		console.log(`Got message: \`${message.text}\` from Telegram-user: ${message.from.username || message.from.first_name} (${message.from.id})`);
+	}
+	dcBot.sendMessage({
+		to: settings.discord.channelID,
+		message: `${message.from.username || message.from.first_name}: ${message.text}`
+	});
+});
+
+if (settings.debug) {
 	process.stdin.setEncoding('utf8');
 	process.stdin.on('data', data => {
 		data = data.trim();
 		switch (data) {
 			case 'exit':
-				tgBotUpdates.stop();
 				process.stdin.pause();
 				console.log('Exiting...');
+				break;
+			case 'help':
+				console.log(
+`Available commands:
+	help - this
+	exit - exit application`
+				);
 				break;
 			default:
 				break;
 		}
-			
 	});
 }
+
--- updategetter.js
+++ updategetter.js
@@ -2,31 +2,41 @@
 
 const EventEmitter = require('events').EventEmitter;
 
-const updateGetter = function updateGetter(bot, timeout = 60) {
+const updateGetter = function updateGetter (bot, timeout = 60) {
 	const emitter = new EventEmitter();
 	let stop = false;
-	emitter.stop = () => stop = true;
+	emitter.stopUpdates = () => { stop = true; };
 	let offset = 0;
 
-        const fetchUpdates = () => {
-                bot.getUpdates(offset ? { timeout, offset } : { timeout })
-                        .then(handleUpdates)
-                        .catch(err => console.error(`${err.name}: ${err.message}`));
-        };
+	const fetchUpdates = () => {
+		bot.getUpdates(offset ? { timeout, offset } : { timeout })
+			.then(handleUpdates)
+			.catch(err => console.error(`${err.name}: ${err.message}`));
+	};
 
 	const handleUpdates = updates => {
 		updates.forEach(update => {
-			if (offset <= update.update_id)
+			if (offset <= update.update_id) {
 				offset = update.update_id + 1;
+			}
 			emitter.emit('update', update);
+			if (typeof update.message !== 'undefined') {
+				emitter.emit('message', update.message);
+			}
+			if (typeof update.message.text !== 'undefined') {
+				emitter.emit('text', update.message);
+			}
 			if (!stop) setTimeout(fetchUpdates);
 		});
 	};
+	for (let k in emitter) {
+		bot[k] = typeof emitter[k] === 'function' ? emitter[k].bind(emitter) : emitter[k];
+	}
 
 	fetchUpdates();
-	return emitter;
+
+	return bot;
 };
 
 module.exports = updateGetter;
-
 
