afe7903f62b68195d6b278a0c52a4b24cd32da8f
Simen de Lange <simen.de.lange@gmail.com>
2016-11-06 21:46:56 +0100

    Far better way to handle the mapping between discord users and IDs

---
+++ DiscordUserMap.js
@@ -0,0 +1,190 @@
+"use strict";
+
+/**************************
+ * Import important stuff *
+ **************************/
+
+const fs = require("fs");
+const _ = require("lodash");
+
+/****************************
+ * The DiscordUserMap class *
+ ****************************/
+
+/**
+ * Handles the mapping between UserID and Username in Discord and saves it to a file when it changes
+ */
+class DiscordUserMap {
+	/**
+	 * Creates a new mapping betweein user IDs and usernames
+	 *
+	 * @param {String} filename	Name of the file read the map from and store it to
+	 */
+	constructor(filename) {
+		/**
+		 * The filename this map is associated with
+		 *
+		 * @private
+	 	 */
+		this._filename = filename;
+
+		try {
+			// Check if the file exists. This throws if it doesn't
+			fs.accessSync(this.filename, fs.constants.F_OK);
+		} catch (e) {
+			// Nope, it doesn't. Create it
+			fs.writeFileSync(this.filename, JSON.stringify({}));
+		}
+
+		// Read the file
+		let data = fs.readFileSync(this.filename);
+
+		// Parse it as JSON
+		data = JSON.parse(data);
+
+		/**
+		 * The mapping between IDs and names
+		 *
+		 * @private
+		 */
+		this._idToName = data;
+
+		/**
+		 * The mapping between names and IDs
+		 *
+		 * @private
+		 */
+		this._nameToId = {};
+		for (let id in data) {
+			this._nameToId[data[id]] = id;
+		}
+
+		/**
+		 * A promise which resolves to nothing when the file is ready for writing
+		 *
+		 * @private
+		 */
+		this._finishedWriting = Promise.resolve();
+	}
+
+	/**
+	 * Writes the current map to file
+	 *
+	 * @private
+	 */
+	_saveMap() {
+		// Create the next "finishedWriting" promise
+		let p = new Promise((resolve, reject) => {
+			// Wait for the previous write operation to finish
+			this._finishedWriting.then(() => {
+
+				// Write the map to file
+				fs.writeFile(this._filename, JSON.stringify(this._idToName, null, "\t"), {encoding: "utf-8"}, err => {
+					if (err) {
+						reject(err);
+					} else {
+						resolve();
+					}
+				});
+			});
+		});
+
+		// Set the new "finishedWriting" promise
+		this._finishedWriting = p;
+	}
+
+	/**
+	 * Maps a username to an ID. Example: nameMap.mapUsername("Foo").toID(123);
+	 *
+	 * @param {String} username	The username to map to an ID
+	 *
+	 * @return {Object}	An object with a 'toID' property, which is used to complete the mapping
+	 */
+	mapUsername(username) {
+		return {
+			toID: id => {
+				// Check if the mapping exists
+				if (this._nameToId[username] !== id) {
+					// Nope. Create or update it
+					this._nameToId[username] = id;
+					this._idToName[id] = username;
+
+					// Save it
+					this._saveMap();
+				}
+			}
+		};
+	}
+
+	/**
+	 * Maps an ID to a username. Example: nameMap.mapID(123).toUsername("Foo");
+	 *
+	 * @param {String} id	The ID to map to a username
+	 *
+	 * @return {Object}	An object with a 'toUsername' property, which is used to complete the mapping
+	 */
+	mapID(id) {
+		return {
+			toUsername: username => {
+				// Check if the mapping exists
+				if (this._idToName[id] !== username) {
+					// Nope. Create or update it
+					this._nameToId[username] = id;
+					this._idToName[id] = username;
+
+					// Save it
+					this._saveMap();
+				}
+			}
+		};
+	}
+
+	/**
+	 * Looks up an ID and returns a username
+	 *
+	 * @param {String} id	The ID to look up
+	 *
+	 * @return {String}	The username this ID belongs to
+	 */
+	lookupID(id) {
+		return this._idToName[id];
+	}
+
+	/**
+	 * Looks up a username and returns an ID
+	 *
+	 * @param {String} username	The name to look up
+	 *
+	 * @return {String}	The ID this username belongs to
+	 */
+	lookupUsername(id) {
+		return this._nameToId[id];
+	}
+
+	/**
+	 * The filename this map is linked to
+	 */
+	get filename() {
+		return this._filename;
+	}
+
+	/**
+	 * The mapping between names and IDs
+	 */
+	get nameToIdMap() {
+		return _.clone(this._nameToId);
+	}
+
+	/**
+	 * The mapping between IDs and names
+	 */
+	get idToNameMap() {
+		return _.clone(this._idToName);
+	}
+}
+
+/********************
+ * Export the class *
+ ********************/
+
+module.exports = DiscordUserMap;
--- main.js
+++ main.js
@@ -13,6 +13,8 @@ const { BotAPI, InputFile } = require("teleapiwrapper");
 const Discord = require("discord.io");
 const updateGetter = require("./updategetter.js");
 
+const DiscordUserMap = require("./DiscordUserMap");
+
 /**************************
  * Read the settings file *
  **************************/
@@ -30,7 +32,7 @@ debug.doDebug = settings.debug;
  * TediCross *
  *************/
 
-// Create a Telegram bot
+// Create a Telegram bot and start longpolling for updates
 const tgBot = new BotAPI(settings.telegram.auth.token);
 updateGetter(tgBot, settings.telegram.timeout);
 
@@ -45,20 +47,17 @@ const dcBot = new Discord.Client({
 dcBot.on("ready", () => console.log(`Discord: ${dcBot.username} (${dcBot.id})`));
 tgBot.getMe().then(bot => console.log(`Telegram: ${bot.username} (${bot.id})`));
 
-// Read the UserID/Username map for Discord
-const dcUsers = JSON.parse(fs.readFileSync(settings.discord.usersfile, "utf8"));
+/***************************
+ * Set up the Discord part *
+ ***************************/
 
-// Update the UserID/Username map every 30 seconds
-setInterval(() => fs.writeFile(settings.discord.usersfile, JSON.stringify(dcUsers, undefined, "\t")), 30000);
-
-/****************
- * From Discord *
- ****************/
+// Mapping between usernames and IDs
+const dcUsers = new DiscordUserMap(settings.discord.usersfile);
 
 dcBot.on("any", e => {
 	if (e.t === "GUILD_CREATE") {
 		e.d.members.forEach(member => {
-			dcUsers[member.user.id] = member.user.username;
+			dcUsers.mapID(member.user.id).toUsername(member.user.username);
 		});
 	} else if (e.t === "MESSAGE_CREATE") {
 		if(e.d.attachments) {
@@ -76,12 +75,17 @@ dcBot.on("any", e => {
 		}
 	}
 });
-dcBot.on("presence", (user, userID) => { dcUsers[userID] = user; });
+
+// Listen for presence
+dcBot.on("presence", (user, userID) => {
+	// Store the UserID/Username mapping
+	dcUsers.mapID(userID).toUsername(user);
+});
 
 // Listen for Discord messages
 dcBot.on("message", (user, userID, channelID, message, event) => {
 	// Store the UserID/Username mapping
-	dcUsers[userID] = user;
+	dcUsers.mapID(userID).toUsername(user);
 
 	// Ignore own messages
 	if (userID !== settings.discord.botID) {
@@ -91,15 +95,15 @@ dcBot.on("message", (user, userID, channelID, message, event) => {
 		tgBot.sendMessage({
 			chat_id: settings.telegram.chat_id,
 			text: `<b>${user}</b>: ${message
-				.replace(/</g, "&lt;")
-				.replace(/>/g, "&gt;")
-				.replace(/&/g, "&amp;")
-				.replace(/\*\*([^*]+)\*\*/g, (m, b) => "<b>" + b + "</b>")
-				.replace(/\*([^*]+)\*/g, (m, b) => "<i>" + b + "</i>")
-				.replace(/_([^*]+)_/g, (m, b) => "<i>" + b + "</i>")
-				.replace(/<@!(\d+)>/g, (m, id) => {
-					if (dcUsers[id]) {
-						return `@${dcUsers[id]}`;
+				.replace(/</g, "&lt;")	// < to &lt;
+				.replace(/>/g, "&gt;")	// > to &gt;
+				.replace(/&/g, "&amp;")	// & to &amp;
+				.replace(/\*\*([^*]+)\*\*/g, (m, b) => "<b>" + b + "</b>")	// **text** to <b>text</b>
+				.replace(/\*([^*]+)\*/g, (m, b) => "<i>" + b + "</i>")	// *text* to <i>text</i>
+				.replace(/_([^*]+)_/g, (m, b) => "<i>" + b + "</i>")	// _text_ to <i>text</i>
+				.replace(/<@!(\d+)>/g, (m, id) => {	// @ID to @Username
+					if (dcUsers.lookupID(id)) {
+						return `@${dcUsers.lookupID(id)}`;
 					} else {
 						return m;
 					}
@@ -109,9 +113,9 @@ dcBot.on("message", (user, userID, channelID, message, event) => {
 	}
 });
 
-/*****************
- * From Telegram *
- *****************/
+/************************
+ * Set up Telegram part *
+ ************************/
 
 // Set up event listener for text messages from Telegram
 tgBot.on("text", message => {
