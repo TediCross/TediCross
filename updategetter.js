2db65ae0baf2ca11379c69e2c4cce89a23387c0a
Simen de Lange <simen.de.lange@gmail.com>
2016-11-06 13:17:58 +0100

    Commented and refactored the update getter

--- updategetter.js
+++ updategetter.js
@@ -1,42 +1,75 @@
-'use strict';
+"use strict";
 
-const EventEmitter = require('events').EventEmitter;
+/**************************
+ * Import important stuff *
+ **************************/
 
-const updateGetter = function updateGetter (bot, timeout = 60) {
+const EventEmitter = require("events").EventEmitter;
+
+/*****************************
+ * The UpdateGetter function *
+ *****************************/
+
+/**
+ * Adds a longpolling update getter to a Telegram bot and mixes an event emitter into the bot
+ *
+ * @param {teleapiwrapper.BotAPI} bot	The bot to get updates for
+ * @param {Integer} [timeout]	Timeout for longpolling. Don't touch if you don't have a reason to
+ */
+function updateGetter(bot, timeout = 60) {
+	// Create an event emitter
 	const emitter = new EventEmitter();
-	let stop = false;
-	emitter.stopUpdates = () => { stop = true; };
+
+	// Offset for which updates to fetch
 	let offset = 0;
 
-	const fetchUpdates = () => {
-		bot.getUpdates(offset ? { timeout, offset } : { timeout })
-			.then(handleUpdates)
-			.catch(err => console.error(`${err.name}: ${err.message}`));
-	};
+	// Function to fetch updates
+	function fetchUpdates() {
+		// Do the fetching
+		bot.getUpdates({timeout, offset})
+		  .then(updates => {
 
-	const handleUpdates = updates => {
-		updates.forEach(update => {
-			if (offset <= update.update_id) {
+			// Iterate over the updates
+			updates.forEach(update => {
+
+				// Update the offset
 				offset = update.update_id + 1;
-			}
-			emitter.emit('update', update);
-			if (typeof update.message !== 'undefined') {
-				emitter.emit('message', update.message);
-			}
-			if (typeof update.message.text !== 'undefined') {
-				emitter.emit('text', update.message);
-			}
-			if (!stop) setTimeout(fetchUpdates);
-		});
-	};
-	for (let k in emitter) {
-		bot[k] = typeof emitter[k] === 'function' ? emitter[k].bind(emitter) : emitter[k];
+
+				// Emit the update
+				emitter.emit("update", update);
+
+				// Check what type of update this is
+				if (update.message !== undefined) {
+
+					// This is a new message
+					emitter.emit("message", update.message);
+
+					// Is it a text message?
+					if (update.message.text !== undefined) {
+						emitter.emit("text", update.message);
+					}
+				}
+
+				// Get more updates!
+				process.nextTick(fetchUpdates);
+			});
+		  })
+		  .catch(err => console.error(`${err.name}: ${err.message}`));
 	}
 
+	// Start the fetching
 	fetchUpdates();
 
-	return bot;
-};
+	// Mix the emitter into the bot
+	for (let k in emitter) {
+		bot[k] = typeof emitter[k] === "function" ? emitter[k].bind(emitter) : emitter[k];
+	}
+
+}
+
+/***********************
+ * Export the function *
+ ***********************/
 
 module.exports = updateGetter;
 
