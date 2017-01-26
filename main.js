dd407f3ac64a7445c6303af0cea4da207727f8d3
Simen de Lange <simen.de.lange@gmail.com>
2017-01-26 21:31:45 +0100

    Removed a bad comment

--- main.js
+++ main.js
@@ -112,9 +112,7 @@ dcBot.on("message", message => {
 			  .replace(/</g, "&lt;")
 			  .replace(/>/g, "&gt;")
 			  .replace(/```\S+\n/g, "```")	// Ignore the language of code blocks. Telegram can't really do anything with that info
-			  .replace(/```((.|\s)+?)```/g, (match, code) => {
-				return `<pre>${code}</pre>`;	// FIXME Doesn't work
-			  })
+			  .replace(/```((.|\s)+?)```/g, (match, code) => `<pre>${code}</pre>`)
 			  .replace(/`([^`]+)`/g, (match, code) => `<code>${code}</code>`)
 			  .replace(/__(.*?)__/g, (match, text) => `<b>${text}</b>`)	// Telegram doesn't support '<u>', so make it bold instead
 			  .replace(/\*\*(.*?)\*\*/g, (match, text) => `<b>${text}</b>`)
