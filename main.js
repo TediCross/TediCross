0195ade25ee79b6b1dab15b6bbfd91fa57f22a86
Simen de Lange <simen.de.lange@gmail.com>
2017-07-20 20:40:36 +0200

    Got rid of some singleton instances

--- main.js
+++ main.js
@@ -4,10 +4,8 @@
  * Import important stuff *
  **************************/
 
+// General stuff
 const Logger = require("./lib/Logger");
-
-// Load the settings
-const settings = require("./lib/settings");
 
 // Telegram stuff
 const { BotAPI, InputFile } = require("teleapiwrapper");
@@ -16,15 +14,22 @@ const telegramSetup = require("./lib/telegram2discord/setup");
 // Discord stuff
 const Discord = require("discord.js");
 const discordSetup = require("./lib/discord2telegram/setup");
+const DiscordUserMap = require("./lib/discord2telegram/DiscordUserMap");
+
+/*************
+ * TediCross *
+ *************/
 
 // Create a logger
 const logger = new Logger();
 
-// Wrap everything in a try/catch to get a timestamp when a crash occurs
+// Wrap everything in a try/catch to get a timestamp if a crash occurs
 try {
-	/*************
-	 * TediCross *
-	 *************/
+	// Load the settings
+	const settings = require("./lib/settings");
+
+	// Create/Load the discord user map
+	const dcUsers = new DiscordUserMap(settings.discord.usersfile);
 
 	// Create a Telegram bot
 	const tgBot = new BotAPI(settings.telegram.auth.token);
@@ -34,19 +39,21 @@ try {
 
 	// Log data when the bots are ready
 	dcBot.on("ready", () => logger.info(`Discord: ${dcBot.user.username} (${dcBot.user.id})`));
-	tgBot.getMe().then(bot => {
+	tgBot.getMe()
+	  .then(bot => {
 		logger.info(`Telegram: ${bot.username} (${bot.id})`)
 
 		// Put the data on the bot
 		tgBot.me = bot;
-	}).catch(err => logger.error("Failed at getting the Telegram bot's me-object:", err));
+	  })
+	  .catch(err => logger.error("Failed at getting the Telegram bot's me-object:", err));
 
 	/*********************
 	 * Set up the bridge *
 	 *********************/
 
-	discordSetup(dcBot, tgBot, logger);
-	telegramSetup(tgBot, dcBot, logger);
+	discordSetup(dcBot, tgBot, logger, settings, dcUsers);
+	telegramSetup(tgBot, dcBot, logger, settings, dcUsers);
 } catch (err) {
 	// Log the timestamp and re-throw the error
 	logger.error(err);
