6203cf07504bb23f7cbb6d652bbd6c81101f14bd
Horațiu Mlendea <Horatiu.Mlendea@ProtonMail.com>
2022-03-09 12:15:56 +0200

    Small fixes

--- src/settings/TelegramSettings.js
+++ src/settings/TelegramSettings.js
@@ -19,7 +19,7 @@ class TelegramSettings {
 	 *
 	 * @param {Object} settings	The raw settings object to use
 	 * @param {String} settings.token	The bot token to use. Set to {@link TelegramSettings#GET_TOKEN_FROM_ENVIRONMENT} to read the token from the TELEGRAM_BOT_TOKEN environment variable
-	 * @param {String} settings.whitelistedSender	The sender whose messages will be relayed. Leave null to relay all messages
+	 * @param {String} settings.whitelistedSender	The sender whose messages will be relayed. Leave empty to relay all messages
 	 * @param {Boolean} settings.useFirstNameInsteadOfUsername	Whether or not to use a Telegram user's first name instead of the username when displaying the name in the Discord messages
 	 * @param {Boolean} settings.colonAfterSenderName	Whether or not to put a colon after the name of the sender in messages from Discord to Telegram. If true, the name is displayed `Name:`. If false, it is displayed `Name`
 	 * @param {Boolean} settings.skipOldMessages	Whether or not to skip through all previous messages cached from the telegram-side and start processing new messages ONLY
@@ -41,7 +41,7 @@ class TelegramSettings {
 		this._token = settings.token;
 
 		/**
-		 * The username whose messages will be relayed. Leave null to relay all messages
+		 * The username whose messages will be relayed. Leave empty to relay all messages
 		 *
 		 * @type {String}
 		 */
@@ -170,7 +170,7 @@ class TelegramSettings {
 	static get DEFAULTS() {
 		return {
 			token: TelegramSettings.GET_TOKEN_FROM_ENVIRONMENT,
-			whitelistedSender: null,
+			whitelistedSender: '',
 			useFirstNameInsteadOfUsername: false,
 			colonAfterSenderName: false,
 			skipOldMessages: true,
