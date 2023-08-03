d46040994af145fbf5a51abddbc5b59a2ba577a2
kutuk <avis@kutuk.ru>
2023-08-03 18:01:21 +0200

    transfer group of files from Discord to Tg as Gallery

--- src/telegram2discord/middlewares.ts
+++ src/telegram2discord/middlewares.ts
@@ -162,6 +162,13 @@ function addMessageObj(ctx: TediCrossContext, next: () => void) {
 		return;
 	}
 
+	// TODO: console
+	console.dir((ctx as any).update);
+
+	if ((ctx as any).update.message.photo) {
+		console.log(JSON.stringify((ctx as any).update.message.photo, null, " "));
+	}
+
 	ctx.tediCross.message = R.cond([
 		// XXX I tried both R.has and R.hasIn as conditions. Neither worked for some reason
 		[ctx => !R.isNil((ctx as any).update.channel_post), R.path(["update", "channel_post"])],
@@ -290,7 +297,7 @@ function informThisIsPrivateBot(ctx: TediCrossContext, next: () => void) {
 						"This is an instance of a [TediCross](https://github.com/TediCross/TediCross) bot, " +
 							"bridging a chat in Telegram with one in Discord. " +
 							"If you wish to use TediCross yourself, please download and create an instance.",
-						{parse_mode: "Markdown"}
+						{ parse_mode: "Markdown" }
 					)
 						.then(msg =>
 							// Delete it again after a while
