359a1afd1b5e312bc3e30c71f2599c1d6156e13f
Lala Sabathil <aiko@aitsys.dev>
2023-08-19 01:47:06 +0200

    fix: set slash perms default to admin-only

--- src/discord2telegram/commands/chatinfo.ts
+++ src/discord2telegram/commands/chatinfo.ts
@@ -1,7 +1,10 @@
-import { SlashCommandBuilder, CommandInteraction } from "discord.js";
+import { SlashCommandBuilder, CommandInteraction, PermissionFlagsBits } from "discord.js";
 
 module.exports = {
-	data: new SlashCommandBuilder().setName("chatinfo").setDescription("Provides information about the chat."),
+	data: new SlashCommandBuilder()
+		.setName("chatinfo")
+		.setDescription("Provides information about the chat.")
+		.setDefaultMemberPermissions(PermissionFlagsBits.Administrator),
 	async execute(interaction: CommandInteraction) {
 		await interaction.reply({ content: "channelId: `" + interaction.channelId + "`.", ephemeral: true });
 	}
--- src/discord2telegram/commands/threadInfo.ts
+++ src/discord2telegram/commands/threadInfo.ts
@@ -1,7 +1,10 @@
-import { SlashCommandBuilder, CommandInteraction } from "discord.js";
+import { SlashCommandBuilder, CommandInteraction, PermissionFlagsBits } from "discord.js";
 
 module.exports = {
-	data: new SlashCommandBuilder().setName("threadinfo").setDescription("Provides information about the thread."),
+	data: new SlashCommandBuilder()
+		.setName("threadinfo")
+		.setDescription("Provides information about the thread.")
+		.setDefaultMemberPermissions(PermissionFlagsBits.Administrator),
 	async execute(interaction: CommandInteraction) {
 		if (interaction.channel?.isThread()) {
 			await interaction.reply({ content: "threadId: `" + interaction.channelId + "`.", ephemeral: true });
